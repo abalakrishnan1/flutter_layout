ed4ec8aed1b566fe9de594ca573e6c7e481c98cd
Advay Balakrishnan <abalakrishnan@warriorlife.net>
2021-07-16 12:31:36 -0700

    edited some things.

--- lib/main.dart
+++ lib/main.dart
@@ -100,6 +100,12 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: Text("Layout Demo")),
         body: ListView(
           children: [
+            Image.asset(
+              "images/lake.jpg",
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
             buttonSection,
             textSection,
